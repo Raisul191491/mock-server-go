2d43a9fe4470c3260d9ccfc46a8d87185926d28e
raisul <raisul.islam@vivasoftltd.com>
2024-05-09 17:48:51 +0600

    code change

--- main.go
+++ main.go
@@ -2,11 +2,7 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"strconv"
 	"time"
-
-	"github.com/gin-gonic/gin"
 )
 
 func fibonacci(n int) int {
@@ -16,84 +12,78 @@ func fibonacci(n int) int {
 	return fibonacci(n-1) + fibonacci(n-2)
 }
 
-func fibonacciHandler(c *gin.Context) {
+func fibonacciHandler(val int) {
 	start := time.Now() // Record the start time
 
-	numStr := c.Param("number")
-	num, err := strconv.Atoi(numStr)
-	if err != nil {
-		c.JSON(400, gin.H{"error": "Invalid number"})
-		return
-	}
-
-	result := fibonacci(num)
+	result := fibonacci(val)
 
 	elapsed := time.Since(start) // Calculate the elapsed time
 
-	// Prepare response with result and latency information
-	c.JSON(200, gin.H{
-		"fibonacci": result,
-		"latency":   elapsed.String(), // Convert elapsed time to string for response
-	})
+	fmt.Println("The ans is for %d is : ", result)
+	fmt.Println("latency :%v", elapsed)
 }
 
-func ping(c *gin.Context) {
-	c.JSON(200, gin.H{"response": "pong"})
-}
+// func ping() {
+// 	c.JSON(200, gin.H{"response": "pong"})
+// }
 
-type Graph struct {
-	Nodes []*Node
-}
+// type Graph struct {
+// 	Nodes []*Node
+// }
 
-type Node struct {
-	ID    int
-	Edges []*Node
-}
+// type Node struct {
+// 	ID    int
+// 	Edges []*Node
+// }
 
-func generateGraph(c *gin.Context) {
-	start := time.Now() // Record the start time
+// func generateGraph() {
+// 	start := time.Now() // Record the start time
 
-	numStr := c.Param("nodes")
-	num, err := strconv.Atoi(numStr)
-	if err != nil {
-		c.JSON(400, gin.H{"error": "Invalid number"})
-		return
-	}
+// 	numStr := c.Param("nodes")
+// 	num, err := strconv.Atoi(numStr)
+// 	if err != nil {
+// 		c.JSON(400, gin.H{"error": "Invalid number"})
+// 		return
+// 	}
 
-	numNodes, numEdgesPerNode := num, num*10
+// 	numNodes, numEdgesPerNode := num, num*10
 
-	graph := &Graph{}
-	for i := 0; i < numNodes; i++ {
-		node := &Node{ID: i}
-		graph.Nodes = append(graph.Nodes, node)
-	}
+// 	graph := &Graph{}
+// 	for i := 0; i < numNodes; i++ {
+// 		node := &Node{ID: i}
+// 		graph.Nodes = append(graph.Nodes, node)
+// 	}
 
-	for _, node := range graph.Nodes {
-		for j := 0; j < numEdgesPerNode; j++ {
-			edgeIdx := rand.Intn(numNodes)
-			node.Edges = append(node.Edges, graph.Nodes[edgeIdx])
-		}
-	}
+// 	for _, node := range graph.Nodes {
+// 		for j := 0; j < numEdgesPerNode; j++ {
+// 			edgeIdx := rand.Intn(numNodes)
+// 			node.Edges = append(node.Edges, graph.Nodes[edgeIdx])
+// 		}
+// 	}
 
-	elapsed := time.Since(start) // Calculate the elapsed time
+// 	elapsed := time.Since(start) // Calculate the elapsed time
 
-	// Prepare response with result and latency information
-	c.JSON(200, gin.H{
-		"result":  "done",
-		"latency": elapsed.String(), // Convert elapsed time to string for response
-	})
-}
+// 	// Prepare response with result and latency information
+// 	c.JSON(200, gin.H{
+// 		"result":  "done",
+// 		"latency": elapsed.String(), // Convert elapsed time to string for response
+// 	})
+// }
 
 func main() {
-	r := gin.Default()
-
-	r.GET("/fibonacci/:number", fibonacciHandler)
-	r.GET("/ping", ping)
-	r.GET("/graph/:nodes", generateGraph)
-
-	fmt.Println("Server listening on port 8080...")
-	err := r.Run(":8080")
-	if err != nil {
-		fmt.Println("Error starting server:", err)
+	testArr := []int{30, 45, 47, 50}
+	for _, val := range testArr {
+		fibonacci(val)
 	}
+	// r := gin.Default()
+
+	// r.GET("/fibonacci/:number", fibonacciHandler)
+	// r.GET("/ping", ping)
+	// r.GET("/graph/:nodes", generateGraph)
+
+	// fmt.Println("Server listening on port 8080...")
+	// err := r.Run(":8080")
+	// if err != nil {
+	// 	fmt.Println("Error starting server:", err)
+	// }
 }
